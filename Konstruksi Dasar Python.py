571f607654a989035ed49b847e87b1d4d745fd10
hani <mustikahani@yahoo.com>
2022-02-02 22:28:27 +0700

    konstruksi dasar

---
+++ Konstruksi Dasar Python.py
@@ -0,0 +1,18 @@
+# Konstruksi Dasar Python
+# Sequential
+print('hello world')
+print('by hani')
+print('tanggal 2 feb 2022')
+
+# Percabangan eksekusi terpilih
+x = False
+if x  :
+      print('Jalan lurus aja')
+else:
+      print('jalan lain')
+
+# Perulangan
+jumlahanak= 4
+
+for index_anak in range(1, jumlahanak+1): #jumlah perulangan = 5 - 1 = 4
+    print(f'Halo anak #{index_anak}')
