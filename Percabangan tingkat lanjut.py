32d453646b0652cbea55cc5d258dd2787109786c
hani <mustikahani@yahoo.com>
2022-02-10 08:11:54 +0700

    Perrcabangan tingkat lanjut

---
+++ Percabangan tingkat lanjut.py
@@ -0,0 +1,12 @@
+jumlah_botol_susu = 5
+jumlah_telor = 2
+
+if jumlah_botol_susu > 0 :
+    print('Budi mengecek dan ternyata uangnya cukup')
+    if jumlah_telor == 0 :
+        print('budi membeli satu botol susu')
+    else:
+        print('budi tidak jadi membeli satu botol susu')
+
+print('budi pulang k rumah')
+print('budi menyampaikan hasilnya k ibu ')
