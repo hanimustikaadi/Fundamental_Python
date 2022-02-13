cbcab353af6cc9484f92943b5e8d936b2ce376ed
hani <mustikahani@yahoo.com>
2022-02-13 17:47:41 +0700

    Perulangan

---
+++ dasar_sintaks_perulangan_While.py
@@ -0,0 +1,15 @@
+"""
+Perulangan Sederhana Menngunaka While
+"""
+
+Jumlah_buku =10
+print('ibu berkata, baca semua bukumu')
+
+jumlah_buku_yang_sudahdibaca = 0
+
+while jumlah_buku_yang_sudahdibaca < Jumlah_buku:
+    jumlah_buku_yang_sudahdibaca = jumlah_buku_yang_sudahdibaca + 1
+    print(f"buku ke {jumlah_buku_yang_sudahdibaca} sudah di baca")
+
+
+print(f"jumlah buku yang sudah di baca {jumlah_buku_yang_sudahdibaca}")
---
+++ dasar_sintaks_perulangan_for.py
@@ -0,0 +1,14 @@
+"""
+Perulangan Sederhana Menngunaka FOR
+"""
+
+Jumlah_buku =10
+print('ibu berkata, baca semua bukumu')
+
+jumlah_buku_yang_sudahdibaca = 0
+
+for jumlah_buku_yang_sudahdibaca in range(1,11):
+    print(f"buku ke {jumlah_buku_yang_sudahdibaca} sudah di baca")
+
+print(jumlah_buku_yang_sudahdibaca
+
